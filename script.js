4cd46675c0e2835ba912acf9b5d0ee9d2349aa5b
Zioo02 <kadzielat@gmail.com>
2023-12-05 16:29:02 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+document.addEventListener('mousemove', function(e) {
+    const glow = document.getElementById('glow');
+    glow.style.left = e.clientX + 'px';
+    glow.style.top = e.clientY + 'px';
+  });
+
+document.addEventListener('DOMContentLoaded', (e) => {
+    const navbar = document.getElementById('portfolio-navbar');
+    const portfolioSection = document.getElementById('my-portfolio');
+  
+    window.addEventListener('scroll', () => {
+      const rect = portfolioSection.getBoundingClientRect();
+      
+      // Sprawdź, czy sekcja portfolio jest widoczna
+      if (rect.top + window.innerHeight <= window.innerHeight) {
+        navbar.style.transform = 'translateY(0)';
+      } else {
+        navbar.style.transform = 'translateY(-100%)';
+      }
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', (event) => {
+    const navbarItems = document.querySelectorAll('#portfolio-navbar p');
+    const sections = document.querySelectorAll('.project-section');
+  
+    function onScroll() {
+      let currentSection = '';
+  
+      sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+        if (sectionTop <= window.innerHeight / 2) { // Ajust this value as needed
+          currentSection = section.getAttribute('id');
+        }
+      });
+  
+      navbarItems.forEach(item => {
+        item.classList.remove('active');
+        if (item.getAttribute('data-target') === currentSection) {
+          item.classList.add('active');
+        }
+      });
+    }
+  
+    window.addEventListener('scroll', onScroll);
+  });
+  
+  
